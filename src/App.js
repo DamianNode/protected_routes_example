74de176efb49944ef4d7e2e1dd46ac6e40a9ee78
Damian Janus <damian.janus@gmail.com>
2021-10-08 22:20:44 +0200

    make example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100)
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+
+const PrivateRoute = ({ children, ...rest }) => {
+  return (
+    <Route {...rest} render={() => {
+      return fakeAuth.isAuthenticated === true
+        ? children
+        : <Redirect to='/login' />
+    }} />
+  )
+}
+
+
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
+const Login = () => {
+  const [redirectToReferrer, setRedirectToReferrer] = useState(false);
+
+  const login = () => {
+    fakeAuth.authenticate(() => {
+      setRedirectToReferrer(true)
+    })
+  }
+
+  if (redirectToReferrer === true) {
+    return <Redirect to='/' />
+  }
+
+  return (
+    <div>
+      <p>You must log in to view Page</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to='/public'>Public Page</Link></li>
+          <li><Link to='/protected'>Protected Page</Link></li>
+        </ul>
+
+        <Route path='/public'>
+          <Public />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <PrivateRoute path='/protected'>
+          <Protected />
+        </PrivateRoute>
+      </div>
+    </Router>
   );
 }
 
